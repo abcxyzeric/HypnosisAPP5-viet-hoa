0597777b7df3b17883e941243e60333615fbe42b
dick <nguyenthang0912@gmail.com>
2026-05-19 22:07:40 +0700

    Pin frontend assets after mount fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@e09f215a97b7189024ca1993e0c927f9ffd848e0/dist/',
+  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@68c705cdaf6976a7e08adc189a413359de48d8cd/dist/',
   plugins: [react()],
   build: {
     sourcemap: false,
