727c5fe3ffaaa796cef41f92b152e9e6b5ee720a
dick <nguyenthang0912@gmail.com>
2026-05-19 22:00:07 +0700

    Pin achievement UI assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@8297adb4a1f156a2102fee048f23346342186c87/dist/',
+  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@e09f215a97b7189024ca1993e0c927f9ffd848e0/dist/',
   plugins: [react()],
   build: {
     sourcemap: false,
