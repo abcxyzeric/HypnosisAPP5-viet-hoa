742fdefa4ca1e8aeb487294eea6ad1264904b570
dick <nguyenthang0912@gmail.com>
2026-05-19 21:12:53 +0700

    Use absolute main CDN assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@card31-viet-hoa-v1/dist/',
+  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@main/dist/',
   plugins: [react()],
   build: {
     sourcemap: false,
