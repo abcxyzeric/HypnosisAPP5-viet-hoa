43455c3aed393e5da0b3cf52a14dd625b70113f9
dick <nguyenthang0912@gmail.com>
2026-05-24 19:07:36 +0700

    Pin jsDelivr assets to fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@main/dist/',
+  base: 'https://cdn.jsdelivr.net/gh/abcxyzeric/HypnosisAPP5-viet-hoa@d79bd76b9aa33eec78886410afaf6316b1f61e9d/dist/',
   plugins: [react()],
   build: {
     sourcemap: false,
